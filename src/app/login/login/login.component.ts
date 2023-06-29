4aa5c4e6affbd1ef45c02a9502ddf1c51b58a3be
eduardomartinez73119@gmail.com <eduardomartinez73119@gmail.com>
2023-06-29 15:51:07 -0500

    Added Animation

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { trigger,state, style, animate, transition } from '@angular/animations';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  animations: [
+    trigger('formAnimation', [
+      state('login', style({ transform: 'translateX(0)' })),
+      state('register', style({ transform: 'translateX(-100%)' })),
+      transition('login <=> register', animate('300ms ease-in-out')),
+    ]),
+  ],
 })
 export class LoginComponent {
 
+  activeForm: string = 'login';
+
+  switchToRegister() {
+    this.activeForm = 'register';
+  }
+
+  switchToLogin() {
+    this.activeForm = 'login';
+  }
 }
